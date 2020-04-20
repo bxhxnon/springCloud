395ad874b9e4a703d0750d5f68ccdd5a59df485d
yangwang <bxhxnon@163.com>
2020-04-20 17:05:39 +0800

    第一次提交

---
+++ springboot_tests/src/main/java/TestMain.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: bxh
+ * @Date: 2020/4/20 17:00
+ * @Description:
+ */
+public class TestMain {
+    public static void main(String[] args){
+         System.out.print("测试");
+    }
+}
